d80bdd3247da4b9b288c6353d8118bcbfc47d776
Masum-Arifin <masumarifinbd@gmail.com>
2022-05-09 23:43:09 +0600

    review

--- src/Pages/Home/Review.js
+++ src/Pages/Home/Review.js
@@ -11,9 +11,9 @@ const Review = ({ review }) => {
         </p>
         <div className="flex items-center">
           <div className="avatar">
-            {/* <div className="w-16 rounded-full ring ring-primary ring-offset-base-100 mr-5">
+            <div className="w-16 rounded-full ring ring-primary ring-offset-base-100 mr-5">
               <img src={review.img} alt="" />
-            </div> */}
+            </div>
           </div>
           <div>
             <h4 className="text-xl">{review.name}</h4>
