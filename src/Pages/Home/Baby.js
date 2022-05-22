4e8346c30337a00597429fa39641701a26ff0999
Masum-Arifin <masumarifinbd@gmail.com>
2022-05-22 23:22:20 +0600

    baby card

--- src/Pages/Home/Baby.js
+++ src/Pages/Home/Baby.js
@@ -7,15 +7,15 @@ const Baby = () => {
       <div className="hero-content flex-col lg:flex-row">
         <img src={baby} className="w-96 rounded-lg shadow-2xl" alt=''/>
         <div className="mx-24">
-          {/* <h1 className="text-5xl font-bold mx-">
+          <h1 className="text-5xl font-bold mx-">
             Exceptional Dental Care, <br /> on Your Terms
-          </h1> */}
-          <p className="py-6">
+          </h1>
+          {/* <p className="py-6">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
             a id nisi.
-          </p>
+          </p> */}
           <button className="btn btn-primary text-white font-bold bg-gradient-to-r from-secondary to-primary">
             Get Started
           </button>
