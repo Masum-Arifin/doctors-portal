6566bc600a7fba51f7038994f2d44f74c69413da
Masum-Arifin <masumarifinbd@gmail.com>
2022-05-24 23:34:28 +0600

    appointment

--- src/Pages/Home/MakeAppointment.js
+++ src/Pages/Home/MakeAppointment.js
@@ -12,15 +12,15 @@ const MakeAppointment = () => {
       </div>
       <div className="flex-1">
         {/* <h3 className="text-3xl text-primary py-3">Appointment</h3> */}
-        <h2 className="text-2xl text-white py-3">Make an Appointment Today</h2>
-        {/* <p className="text-white">
+        {/* <h2 className="text-2xl text-white py-3">Make an Appointment Today</h2> */}
+        <p className="text-white">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. A, corrupti.
           Nihil laboriosam ullam <br /> harum porro quos, quia veniam saepe corrupti
           Nihil laboriosam ullam harum porro <br /> quos, quia veniam saepe corrupti
           Nihil laboriosam ullam harum porro quos, <br /> quia veniam saepe corrupti
           iure unde doloremque perferendis mollitia excepturi eveniet totam.
           Quis, at?
-        </p> */}
+        </p>
         <div className="mt-8">
         <PrimaryBtn>Get Started</PrimaryBtn>
         </div>
