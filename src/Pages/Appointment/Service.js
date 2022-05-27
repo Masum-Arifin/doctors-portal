27879e081681dd201e17c68c808a4c378033f992
Masum-Arifin <masumarifinbd@gmail.com>
2022-05-27 23:49:00 +0600

    new code

--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -5,7 +5,7 @@ const Service = ({service, setTreatment}) => {
     return (
         <div class="card lg:max-w-lg bg-base-100 shadow-xl">
         <div class="card-body text-center">
-          <h2 class="card-title text-secondary">{name}</h2>
+          <h2 class="text-xl font-bold text-secondary">{name}</h2>
           <p>{
               slots.length > 0
               ? <span>{slots[0]}</span> 
@@ -17,7 +17,7 @@ const Service = ({service, setTreatment}) => {
              for="booking-modal"
             disabled={slots.length === 0} 
             onClick={() => setTreatment(service)}
-            class="btn btn-secondary text-white uppercase">Book Appointment</label>
+            class="btn btn-sm btn-secondary text-white uppercase bg-gradient-to-r from-secondary to-primary">Book Appointment</label>
           </div>
         </div>
       </div>
