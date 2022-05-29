4ac6aae6d39af1568af80256bf1f5a989b6fea5e
Masum-Arifin <masumarifinbd@gmail.com>
2022-05-29 22:29:46 +0600

    contact

--- src/Pages/Home/Contact.js
+++ src/Pages/Home/Contact.js
@@ -8,9 +8,9 @@ const Contact = () => {
         background:`url(${appointment})`
     }} className='bg-primary px-10 py-14 '>
       <div className='text-center pb-14 text-white'>
-        {/* <p className='text-xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-accent to-secondary'>
+        <p className='text-xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-accent to-secondary'>
           Contact Us
-        </p> */}
+        </p>
         <h1 className='text-4xl'>Stay connected with us</h1>
       </div>
       <div className='grid grid-cols-1 justify-items-center gap-5'>
