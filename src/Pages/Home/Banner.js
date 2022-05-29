3b4c62d877dc1f944cde4e8d0ea3e2db17cb5e36
Masum-Arifin <masumarifinbd@gmail.com>
2022-05-29 22:27:35 +0600

    services

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -9,11 +9,11 @@ const Banner = () => {
         <img src={chair} className="max-w-sm rounded-lg shadow-2xl" />
         <div>
           <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
-          <p className="py-6">
+          {/* <p className="py-6">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
             a id nisi.
-          </p>
+          </p> */}
           <PrimaryBtn>Get Starts</PrimaryBtn>
         </div>
       </div>
