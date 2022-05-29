768b85ca10e9e29b863954893b0b4f7d239db92e
Masum-Arifin <masumarifinbd@gmail.com>
2022-05-29 22:28:15 +0600

    img

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -5,18 +5,18 @@ import PrimaryBtn from "../Shared/PrimaryBtn";
 const Banner = () => {
   return (
     <div className="hero min-h-screen">
-      <div className="hero-content flex-col lg:flex-row-reverse">
+      {/* <div className="hero-content flex-col lg:flex-row-reverse">
         <img src={chair} className="max-w-sm rounded-lg shadow-2xl" />
         <div>
           <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
-          {/* <p className="py-6">
+          <p className="py-6">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
             a id nisi.
-          </p> */}
+          </p>
           <PrimaryBtn>Get Starts</PrimaryBtn>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
