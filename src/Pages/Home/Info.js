408f91d7c4827e5fd62972a74dea017ec3c1bf3c
Masum-Arifin <masumarifinbd@gmail.com>
2022-06-01 23:17:00 +0600

    info card

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -7,11 +7,11 @@ import phone from "../../assets/icons/phone.svg";
 const Info = () => {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-3 gap-12">
-      <InfoCard
+      {/* <InfoCard
         cardTitle="Opening Hours"
         bgClass="bg-gradient-to-r from-secondary to-primary"
         img={marker}
-      ></InfoCard>
+      ></InfoCard> */}
       <InfoCard
         cardTitle="Our Location"
         bgClass="bg-[#3a4256]"
