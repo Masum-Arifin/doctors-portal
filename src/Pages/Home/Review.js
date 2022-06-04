8b049a1b0630f7d8280eaaaaf42d111b02139bf3
Masum-Arifin <masumarifinbd@gmail.com>
2022-06-04 23:36:04 +0600

    review

--- src/Pages/Home/Review.js
+++ src/Pages/Home/Review.js
@@ -4,11 +4,11 @@ const Review = ({ review }) => {
   return (
     <div className="card lg:max-w-lg bg-base-100 shadow-xl">
       <div className="card-body">
-        <p>
+        {/* <p>
           If a dog chews shoes whose shoes does he choose If a dog chews shoes
           whose shoes does he chooseIf a dog chews shoes whose shoes does he
           choose?
-        </p>
+        </p> */}
         <div className="flex items-center">
           <div className="avatar">
             <div className="w-16 rounded-full ring ring-primary ring-offset-base-100 mr-5">
