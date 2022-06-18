d1269d239599e6c1ba950f3d93536d0c5621a7ff
Masum-Arifin <masumarifinbd@gmail.com>
2022-06-18 22:59:20 +0600

    RequireAuth

--- src/Pages/Login/RequireAuth.js
+++ src/Pages/Login/RequireAuth.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 const RequireAuth = ({children}) => {
-    const [user, loading] = useAuthState(auth);
+    const [user, loading] = useAuthState(aut);
     return (
         <div>
             
