5ab52b7028529c60fb8cc7923220413b26d4fe37
Masum-Arifin <masumarifinbd@gmail.com>
2022-06-18 23:01:24 +0600

    RequireAuth

--- src/Pages/Login/RequireAuth.js
+++ src/Pages/Login/RequireAuth.js
@@ -6,7 +6,9 @@ const RequireAuth = ({children}) => {
     const [user, loading] = useAuthState(auth);
     const location = useLocation();
 
-    
+    if(loading){
+        return <Loading></Loading>
+    }
     return (
         <div>
             
