9262d3ac606aa9c43e6a472ce8ec2bbb611136a0
Masum-Arifin <masumarifinbd@gmail.com>
2022-06-21 18:23:05 +0600

    bookingmodal

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -4,7 +4,7 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from './../../firebase.init';
 import { toast } from 'react-toastify';
 
-const BookingModal = ({ date, setTreatment, treatment }) => {
+// const BookingModal = ({ date, setTreatment, treatment }) => {
 //   const { _id, name, slots } = treatment;
   // const [user, loading, error] = useAuthState(auth);
   // const formattedDate = format(date, 'PP');
