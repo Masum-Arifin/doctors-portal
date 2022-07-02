1d061c8bfcb33da9424310ab5e7006dae76c5975
Masum-Arifin <masumarifinbd@gmail.com>
2022-07-02 17:40:48 +0600

    new code

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -67,7 +67,7 @@ import { toast } from 'react-toastify';
 //               type="text"
 //               name="name"
 //               placeholder="Your Name"
-//               class="input input-bordered w-full max-w-xs"
+              class="input input-bordered w-full max-w-xs"
 //             />
 //             <input
 //               type="email"
